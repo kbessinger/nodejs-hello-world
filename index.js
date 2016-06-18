2fc3c77ce6f61c7b2a70d3edb13228c4916ee2c3
Keith Bessinger <Keith Bessinger>
2016-06-18 06:01:13 -0500

    test simple s3 upload

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
 exports.handler = function(event, context) {
-    context.succeed("Hi Friend");
+  var s3 = new AWS.S3();
+  var param = {Bucket: 'flow-logs', Key: 'test-lamda-x', Body: 'me me me'};
+  console.log("s3");
+  s3.upload(param, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+    console.log('actually done!');
+    context.done();
+  });
+
+  console.log('done?');
+  
 };
